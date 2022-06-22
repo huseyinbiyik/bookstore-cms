edb9b54df1eb6f8191979b22604804b8a87ef930
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-06-22 21:18:32 +0300

    Add adding new  book functionality

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,6 +4,7 @@ import Book from './Book';
 
 export default function Books() {
   const data = useSelector((state) => state.bookReducer);
+  console.log(data);
   return (
     data.map((book) => <Book key={book.id} title={book.title} author={book.author} />)
 
