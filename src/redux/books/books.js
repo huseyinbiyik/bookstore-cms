67084d95e9bfcea8117fe0b427be73d21ef5dfc1
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-06-23 23:15:29 +0300

    Add remove book functionality

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,18 +1,6 @@
-// import { v4 as uuidv4 } from 'uuid';
-
 const apiUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/6d2XiqbsGJ6zQsrtUdmp';
 
-// const BOOK_ADDED = 'bookstore/books/ADD_BOOK';
-const BOOK_REMOVED = 'bookstore/books/BOOK_REMOVED';
 const BOOK_DISPLAYED = 'bookstore/books/BOOK_DISPLAYED';
-
-// export function bookAdded(book) {
-//   return { type: BOOK_ADDED, payload: book };
-// }
-
-export function bookRemoved(book) {
-  return { type: BOOK_REMOVED, payload: book };
-}
 
 export const bookDisplayed = (data) => ({
   type: BOOK_DISPLAYED,
@@ -45,14 +33,25 @@ export const bookAdded = (id, title, author, category = '-') => (
   }
 );
 
+export const bookRemoved = (id) => (
+  (dispatch) => {
+    fetch(`${apiUrl}/books/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        item_id: id,
+      }),
+    })
+      .then(() => dispatch(fetchBook()));
+  }
+);
+
 export default function bookReducer(state = {}, action) {
   switch (action.type) {
     case BOOK_DISPLAYED:
       return action.data;
-    // case BOOK_ADDED:
-    //   return [...state, action.payload];
-    // case BOOK_REMOVED:
-    //   return [...state.filter((book) => book.id !== action.payload.id)];
     default:
       return state;
   }
