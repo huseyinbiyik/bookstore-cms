c5695ed09eda38d7d8acfffe12305b2c5d9b115d
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-06-24 14:36:49 +0300

    Add book card left section

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { bookRemoved } from '../redux/books/books';
+import './Book.css';
 
 const Book = () => {
   const data = useSelector((state) => state.bookReducer);
@@ -11,20 +12,43 @@ const Book = () => {
   };
 
   return (
-    <ul>
+    <div className="book-container">
       {
         Object.keys(data).map((book) => (
-          <li key={book}>
-            <p>{data[book][0].title}</p>
-            <p>{data[book][0].author}</p>
-            <button type="button" onClick={() => handleClick(book)}>
-              Remove
-            </button>
-          </li>
+          <div key={book} className="single-book-container">
+            <div className="left-panel">
+              <ul>
+                <li>
+                  <p className="category">{data[book][0].category}</p>
+                  <p className="book-title">{data[book][0].title}</p>
+                  <p className="book-author">{data[book][0].author}</p>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <button className="remove-button" type="button">
+                    Comments
+                  </button>
+                </li>
+                <li>
+                  <button className="remove-button" type="button" onClick={() => handleClick(book)}>
+                    Remove
+                  </button>
+                </li>
+                <li>
+                  <button className="remove-button" type="button">
+                    Edit
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div className="right-panel" />
+
+            ;
+          </div>
         ))
 }
-
-    </ul>
+    </div>
   );
 };
 
